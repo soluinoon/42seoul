8e6f2ad78cd9cec4eb3f85c34e5fab9b57a48583
eyeofsol <kingjihong@naver.com>
2022-07-02 11:22:14 +0900

    a

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,5 +1,33 @@
-#include "get_next_line.h"
+//#include "get_next_line.h"
 #define BUFFER_SIZE 100
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+
+// strjoin, strchr
+
+char    *
+
+char    *make_line(char *mem, int fd)
+{
+    int     read_count;
+    char    *buffer;
+
+    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buffer)
+        return (NULL);
+    while (strchr(buffer, ' '))
+    read_count = read(fd, buffer, BUFFER_SIZE);
+    if (!read_count)
+    {
+        return (NULL);
+        free(buffer);
+    }
+    buffer[read_count] = '\0';
+    ft_strjoin(mem, buffer);
+    
+    free(buffer);
+}
 
 char	*get_next_line(int fd)
 {
@@ -11,17 +39,6 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || fd > sysconf(OPEN_MAX) || fd == 1 || fd == 2)
 		return (NULL);
-	read_size = read(fd, buff, BUFFER_SIZE);
-	if (!read_size)
-		return (NULL);
-	while (*buff != '\n' || *buff != EOF)
-	{
-		
-	mem = buff;
-	str = (char *)malloc(sizeof(char) * (read_size + 1));
-	if (!str)
-		return (NULL);
-	while (
+	
 
-	return (str);
 }	
